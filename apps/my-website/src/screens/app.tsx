525e5c98a8d979a96bfda0f75643039b61b4095e
DavidTaylor96 <david_taylor96@outlook.com>
2021-11-03 20:44:52 +0000

    feat: hello world!

---
+++ apps/my-website/src/screens/app.tsx
@@ -0,0 +1,11 @@
+import styles from './app.module.scss';
+
+export function App() {
+  return (
+    <div className={styles.app}>
+      <h1>Hello This is going to be my new app</h1>
+    </div>
+  );
+}
+
+export default App;
