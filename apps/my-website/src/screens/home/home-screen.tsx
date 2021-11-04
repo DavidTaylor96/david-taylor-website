2600c5c09b2a2528426abfb9d1097fbe7edf510e
DavidTaylor96 <david_taylor96@outlook.com>
2021-11-04 22:08:05 +0000

    feat: created the home screen so that is scroll to each item

--- apps/my-website/src/screens/home/home-screen.tsx
+++ apps/my-website/src/screens/home/home-screen.tsx
@@ -1,11 +1,18 @@
 import { WelcomeHeader } from '../../components/welcome/welcome-header';
 import './home-screen.scss';
 
-
 export function HomeScreen() {
   return (
     <div className="app">
-      <WelcomeHeader />
+      <header className="wrapper zero">
+        <WelcomeHeader />
+      </header>
+      <header className="wrapper one">
+        <WelcomeHeader />
+      </header>
+      <header className="wrapper two">
+        <WelcomeHeader />
+      </header>
     </div>
   );
 }
