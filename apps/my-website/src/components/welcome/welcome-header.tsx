bf8cd86b1837f523a52001f7d0d8215576ea04d7
DavidTaylor96 <david_taylor96@outlook.com>
2021-11-04 21:56:20 +0000

    feat: added styles to the welcome header component

--- apps/my-website/src/components/welcome/welcome-header.tsx
+++ apps/my-website/src/components/welcome/welcome-header.tsx
@@ -3,9 +3,9 @@ import './welcome-header.scss'
 
 export const WelcomeHeader = () => {
   return (
-    <header >
-      <h1>Hello, my name is</h1>
-      <h3><i>creative developer</i></h3>
+    <header className='welcome-header' >
+      <h1 id="h1">Hello, my name is</h1>
+      <h3 id="h3"><i>creative developer</i></h3>
     </header>
   )
 }
