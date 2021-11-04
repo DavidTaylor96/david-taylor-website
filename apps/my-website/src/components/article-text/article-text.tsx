338c00a1a682ede068cddf398430081f22b0c4b4
DavidTaylor96 <david_taylor96@outlook.com>
2021-11-04 22:31:02 +0000

    feat: added props

--- apps/my-website/src/components/article-text/article-text.tsx
+++ apps/my-website/src/components/article-text/article-text.tsx
@@ -1,30 +1,22 @@
 import './article-text.scss';
-import React from 'react'
+import React from 'react';
 
-interface IArticleScreenProps{
+interface IArticleScreenProps {
   articleHeader: string;
-  articleText: string;
   button?: boolean;
-
 }
 
-
-export const ArticleScreen: React.FC<IArticleScreenProps>  = (props) => {
+export const ArticleScreen: React.FC<IArticleScreenProps> = (props) => {
   return (
     <article className="article-wrapper">
-      <h1>About me</h1>
+      <h1>{props.articleHeader}</h1>
       <span id="separator" />
-      <p>
-        Innovative and self-motivated software developer with good
-        problem-solving skills and a strong desire for continuous learning.
-        committed to continuing my progress and eager to develop my professional
-        development.
-      </p>
-    <button className="button">
-      <p className="button-text">Learn more</p>
-    </button>
+      <p>{props.children}</p>
+      {props.button && (
+        <button className="button">
+          <p className="button-text">Learn more</p>
+        </button>
+      )}
     </article>
   );
 };
-
-
