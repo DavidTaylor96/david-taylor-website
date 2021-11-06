e9ccd80ba0f3049d119ad9e1aa4b5b214404cb42
DavidTaylor96 <david_taylor96@outlook.com>
2021-11-06 11:07:17 +0000

    feat: added more styles to the home page

--- apps/my-website/src/screens/home/home-screen.tsx
+++ apps/my-website/src/screens/home/home-screen.tsx
@@ -1,20 +1,55 @@
 import { ArticleScreen } from '../../components/article-text/article-text';
+import { ImageContainer } from '../../components/image-container/image-container';
 import { WelcomeHeader } from '../../components/welcome/welcome-header';
+import profileMain from '../../assets/profile-main.svg';
+import computer from '../../assets/computer-books-and-plant.svg';
+
 import './home-screen.scss';
+import { FindOutMore } from '../../components/find-out-more/find-out-more';
 
 export function HomeScreen() {
   return (
-    <div className="app">
-      <header className="wrapper zero">
-        <WelcomeHeader />
+    <section className="app">
+      <header className="wrapper main-wrapper">
+        <div className="title-wrapper">
+          <WelcomeHeader />
+        </div>
       </header>
-      <header className="wrapper one">
-        <ArticleScreen />
-      </header>
-      <header className="wrapper two">
-        <WelcomeHeader />
-      </header>
-    </div>
+        <div className="navigator-wrapper-left">
+          <FindOutMore
+          buttonText={'Find out more'}
+           />
+        </div>
+      <article className="wrapper main-wrapper">
+        <div className="article-container">
+          <ArticleScreen articleHeader={'About Me'} button={true}>
+            Innovative and self-motivated software developer with good
+            problem-solving skills and a strong desire for continuous learning.
+            committed to continuing my progress and eager to develop my
+            professional development.
+          </ArticleScreen>
+          <div className="spacer" />
+          <ImageContainer src={profileMain} />
+        </div>
+      </article>
+        <div className="navigator-wrapper-right">
+          <FindOutMore 
+          buttonText={'About my work'}
+          />
+        </div>
+      <article className="wrapper main-wrapper">
+        <div className="article-container">
+          <ImageContainer src={computer} />
+          <div className="spacer" />
+          <ArticleScreen articleHeader={'About my work'} button={true}>
+            I believe in a proactive approch to software deveopment.
+            Understanding the bigger picture and making sure everyone is on the
+            same page. This allows you to react and be agile in your approch to
+            software.
+          </ArticleScreen>
+        </div>
+      </article>
+    </section>
   );
 }
 
