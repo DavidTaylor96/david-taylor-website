262092c7d48456d28cc48e5af16f430e953bc420
DavidTaylor96 <david_taylor96@outlook.com>
2021-11-06 11:19:44 +0000

    feat: for the conditional

--- apps/my-website/src/components/find-out-more/find-out-more.tsx
+++ apps/my-website/src/components/find-out-more/find-out-more.tsx
@@ -4,12 +4,19 @@ import './find-out-more.scss';
 interface IFindOutMore {
   buttonText: string;
   onClick?: () => void;
+  onRight?: boolean;
 }
 
 export const FindOutMore = (props: IFindOutMore) => {
   return (
-    <div className="find-out-more-container">
-      <div>
+    <div
+      className={`${
+        props.onRight
+        ? 'wrapper-right-find-out-more'
+        : 'wrapper-left-find-out-more'
+      }`}
+    >
+      <div className="inner-container">
         <button className="small-button" onClick={props.onClick}>
           <p id="small-button-text">{props.buttonText}</p>
           <FiArrowDown id="small-icon" />
