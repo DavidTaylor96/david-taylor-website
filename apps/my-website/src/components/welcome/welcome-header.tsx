c293fea977be0c981d43af0f67733513a6a9029f
DavidTaylor96 <david_taylor96@outlook.com>
2021-11-08 20:36:30 +0000

    feat: added name logo

--- apps/my-website/src/components/welcome/welcome-header.tsx
+++ apps/my-website/src/components/welcome/welcome-header.tsx
@@ -1,10 +1,11 @@
 import './welcome-header.scss'
-
+import NameLogo from '../../assets/name-logo.svg'
 
 export const WelcomeHeader = () => {
   return (
     <header className='welcome-header' >
       <h1 id="h1">Hello, my name is</h1>
+      <img id="name-logo" src={NameLogo} alt="image of david taylor" />
       <h3 id="h3"><i>creative developer</i></h3>
     </header>
   )
