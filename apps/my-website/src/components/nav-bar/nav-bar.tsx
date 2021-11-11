76decbb07d8254f20cb06f41c6701b589ed6730a
DavidTaylor96 <david_taylor96@outlook.com>
2021-11-11 21:34:45 +0000

    feat: added new path and added new hover style

--- apps/my-website/src/components/nav-bar/nav-bar.tsx
+++ apps/my-website/src/components/nav-bar/nav-bar.tsx
@@ -5,35 +5,26 @@ import './nav-bar.scss';
 
 export const NavBar = () => {
   return (
-    <Flex auto style={{backgroundColor: 'white'}}>
-      <NavLink style={{ display: 'flex' }} to="/" activeClassName="selected">
+    <Flex auto style={{ backgroundColor: 'white' }}>
+      <NavLink style={{ display: 'flex' }} to="/">
         <Flex auto justify="center" align="center">
           <img id="image-logo" src={Logo} alt="image of david taylor" />
         </Flex>
       </NavLink>
       <ul className="nav-bar">
-        <NavLink to="/About" activeClassName="selected">
+        <NavLink to="/" activeClassName="selected">
+          <li className="list-item sliding">
+              Home
+          </li>
+        </NavLink>
+        <NavLink to="/about" activeClassName="selected">
           <li className="list-item sliding">About</li>
         </NavLink>
         <NavLink to="/work" activeClassName="selected">
           <li className="list-item sliding">Work</li>
         </NavLink>
+        <li className="sliding contact">Contact</li>
       </ul>
-      <Flex ml={10} align={'center'} justify={'center'} column>
-        <Flex
-          px={30}
-          py={10}
-          style={{
-            borderRadius: 10,
-            backgroundColor: '#557381',
-            color: 'white',
-            fontFamily: 'Poppins',
-            cursor: 'pointer',
-          }}
-        >
-          Hire me
-        </Flex>
-      </Flex>
     </Flex>
   );
 };
